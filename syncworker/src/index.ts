a74994e72e1535d0964a299c5649582451d2b8d4
blck-snwmn <whitesnowmancreator@gmail.com>
2023-04-01 21:32:44 +0900

    Connect  websocket in the durable object's fetch

--- syncworker/src/index.ts
+++ syncworker/src/index.ts
@@ -19,69 +19,55 @@ export default {
 		env: Env,
 		ctx: ExecutionContext
 	): Promise<Response> {
+		const id = env.ROOM.idFromName("room")
+		const obj = env.ROOM.get(id)
+
+		return obj.fetch(request)
+	},
+};
+
+
+
+export class Room {
+	state: DurableObjectState
+	sessions: WebSocket[] = []
+	env: Env
+
+	constructor(state: DurableObjectState, env: Env) {
+		this.state = state;
+		this.env = env;
+	}
+
+	async fetch(request: Request): Promise<Response> {
 		const upgradeHeader = request.headers.get('Upgrade');
 		if (!upgradeHeader || upgradeHeader !== 'websocket') {
 			return new Response('Expected Upgrade: websocket', { status: 426 });
 		}
 
 		const webSocketPair = new WebSocketPair();
-		const client = webSocketPair[0]
-		const server = webSocketPair[1];
+		const { 0: client, 1: server } = webSocketPair
+
 
 		server.accept();
+		this.sessions.push(server)
+
 		server.addEventListener('message', async event => {
-			const id = env.ROOM.idFromName("room")
-			const obj = env.ROOM.get(id)
+			try {
+				let value: number = await this.state.storage?.get("value") || 0
+				value++
+				await this.state.storage?.put("value", value);
 
-			const resp = await obj.fetch(new Request(request.url))
-			const txt = await resp.text()
-
-			console.log(event.data);
-			server.send("Hello from Cloudflare Workers! You said: " + event.data + ":" + txt)
+				console.log(event.data);
+				const mst = "Hello from Cloudflare Workers! You said: " + event.data + ":" + value
+				this.sessions.forEach(s => s.send(mst))
+			} catch (error) {
+				console.log(error)
+			}
 		});
 
 		return new Response(null, {
 			status: 101,
 			webSocket: client,
 		});
-	},
-};
-
-
-export class Room {
-	state: DurableObjectState
-	constructor(state: DurableObjectState, env: Env) {
-		this.state = state;
-	}
-
-	async fetch(request: Request) {
-		// Apply requested action.
-		let url = new URL(request.url);
-
-		// Durable Object storage is automatically cached in-memory, so reading the
-		// same key every request is fast. (That said, you could also store the
-		// value in a class member if you prefer.)
-		let value: number = await this.state.storage?.get("value") || 0;
-		switch (url.pathname) {
-			case "/increment":
-				++value;
-				break;
-			case "/decrement":
-				--value;
-				break;
-			case "/":
-				// Just serve the current value. No storage calls needed!
-				break;
-			default:
-				return new Response("Not found", { status: 404 });
-		}
-
-		// We don't have to worry about a concurrent request having modified the
-		// value in storage because "input gates" will automatically protect against
-		// unwanted concurrency. So, read-modify-write is safe. For more details,
-		// see: https://blog.cloudflare.com/durable-objects-easy-fast-correct-choose-three/
-		await this.state.storage?.put("value", value);
-
-		return new Response(value.toString());
 	}
 }
