1c83e216f9ab276928efcd9eac9e9c07010691d2
blck-snwmn <whitesnowmancreator@gmail.com>
2023-04-01 20:33:15 +0900

    Loop

--- client/main.go
+++ client/main.go
@@ -1,15 +1,17 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/gorilla/websocket"
 )
 
 func main() {
 	// WebSocket接続先のURLを指定します。
-	url := "ws://localhost:8787/increment"
+	url := "ws://localhost:8787"
 
 	// WebSocketのダイアル設定を作成します。
 	dialer := &websocket.Dialer{}
@@ -21,19 +23,24 @@ func main() {
 	}
 	defer conn.Close()
 
-	// メッセージを送信します。
-	message := "Hello, WebSocket!"
-	err = conn.WriteMessage(websocket.TextMessage, []byte(message))
-	if err != nil {
-		log.Fatal("Failed to send message:", err)
-	}
+	go func() {
+		// メッセージを受信します。
+		_, receivedMessage, err := conn.ReadMessage()
+		if err != nil {
+			log.Fatal("Failed to receive message:", err)
+		}
 
-	// メッセージを受信します。
-	_, receivedMessage, err := conn.ReadMessage()
-	if err != nil {
-		log.Fatal("Failed to receive message:", err)
+		// 受信したメッセージを出力します。
+		fmt.Printf("Received message: %s\n", receivedMessage)
+	}()
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		scanner.Scan()
+		// メッセージを送信します。
+		message := scanner.Text()
+		err = conn.WriteMessage(websocket.TextMessage, []byte(message))
+		if err != nil {
+			log.Fatal("Failed to send message:", err)
+		}
 	}
-
-	// 受信したメッセージを出力します。
-	fmt.Printf("Received message: %s\n", receivedMessage)
 }
